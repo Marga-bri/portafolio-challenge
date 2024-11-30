b0325c0ff513038406fe81f1511a37de53dc21a6
Marga-bri <margarita.bq@hotmail.com>
2024-11-30 16:32:17 -0600

    Versión final del portafolio - ONE

---
+++ js/app.js
@@ -0,0 +1,27 @@
+//La validación del formulário con javascript es considerada un desafío extra, no es obligatório realizar esta validación para realizar su entrega*/
+
+import { validaInputs, validaTextAreas } from "./validaciones.js";
+
+
+const inputs = document.querySelectorAll("input");
+const textareas = document.querySelectorAll("textarea");
+const botonDescargaCv = document.querySelector(".title__network__item--descargaCV")
+
+inputs.forEach((input) => {
+    input.addEventListener("blur", (input) => {
+    validaInputs(input.target);
+});
+});
+
+textareas.forEach((textarea) => {
+    textarea.addEventListener("blur", (textarea) => {
+    validaTextAreas(textarea.target);
+});
+});
+
+
+botonDescargaCv.addEventListener("click", () => {
+    window.open('./assets/CV/Profile CV mbrizuela.pdf');
+});
+
+
